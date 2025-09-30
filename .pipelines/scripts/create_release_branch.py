67bc8a9bedd0f567110591db98cc6efe372de55b
yonik-lr <yonik@lightrun.com>
2025-09-30 16:58:09 +0300

    add logic

--- .pipelines/scripts/create_release_branch.py
+++ .pipelines/scripts/create_release_branch.py
@@ -3,6 +3,8 @@ import sys
 from datetime import datetime, timedelta
 from azure.devops.connection import Connection
 from msrest.authentication import BasicAuthentication
+from azure.devops.v7_0.release.models import VariableValue, VariableGroupProjectReference, ProjectReference
+from azure.devops.v7_0.task_agent.models import VariableGroupParameters
 
 
 def parse_date(date_str):
@@ -10,67 +12,74 @@ def parse_date(date_str):
     return datetime.strptime(date_str, "%Y-%m-%d").date()
 
 
-def get_release_variables(pat: str):
-    organization_url = "https://dev.azure.com/athenat"
-    credentials = BasicAuthentication('', pat)
-    connection = Connection(base_url=organization_url, creds=credentials)
-    task_agent_client = connection.clients.get_task_agent_client()
-
-    all_vars = task_agent_client.get_variable_groups(project="athena", group_name="lightrun-helm-chart-variables")[0].variables
-    next_release_date = all_vars['NEXT_RELEASE_DATE'].value
-    override_release_date = all_vars['OVERRIDE_RELEASE_DATE'].value
-
-    print(f"next release date is {next_release_date} and the override is {override_release_date}")
-    return next_release_date, override_release_date
+def get_variable_group(pat: str, client):
+    variable_group = task_agent_client.get_variable_groups(project="athena", group_name="lightrun-helm-chart-variables")[0]
+    return variable_group
 
 
 def main():
     # today = datetime.utcnow().date()  # current UTC date
     
     if len(sys.argv) < 2:
-        print("Usage: create_release_branch.py <AZURE_DEVOPS_PAT_RO>")
+        print("Usage: create_release_branch.py <AZURE_DEVOPS_PAT_RW>")
         sys.exit(1)
 
     pat = sys.argv[1]
     
-    next_release_date, override_release_date = get_release_variables(pat=pat)
-    
+    organization_url = "https://dev.azure.com/athenat"
+    credentials = BasicAuthentication('', pat)
+    connection = Connection(base_url=organization_url, creds=credentials)
+    task_agent_client = connection.clients.get_task_agent_client()
+
+    variable_group = get_variable_group(pat=pat)
+    next_release_date = variable_group.variables['NEXT_RELEASE_DATE'].value
+    override_release_date = variable_group.variables['OVERRIDE_RELEASE_DATE'].value
+
+    if not next_release_date:
+        print("NEXT_RELEASE_DATE is not set, exiting...")
+        sys.exit(1)
+
     print(f"NEXT_RELEASE_DATE = {next_release_date}")
-    print(f"OVERRIDE_RELEASE_DATE = {override_release_date}")
     
     if not override_release_date:
         print("OVERRIDE_RELEASE_DATE is not set")
+    else:
+        print(f"OVERRIDE_RELEASE_DATE = {override_release_date}")
     
+    # Convert strings to date objects if they aren't already
+    if isinstance(next_release_date, str):
+        next_release_date = parse_date(next_release_date)
+    if isinstance(override_release_date, str):
+        override_release_date = parse_date(override_release_date)
+    
+    # Check the difference in days
+    delta_days = abs((next_release_date - override_release_date).days)
+    if delta_days > 13:
+        print(f"OVERRIDE_RELEASE_DATE ({override_release_date}) is {delta_days} days away from NEXT_RELEASE_DATE ({next_release_date}). Max delta allowed is 13 days. Deleting value of OVERRIDE_RELEASE_DATE")
+        
+        if "OVERRIDE_RELEASE_DATE" in variable_group.variables:
+            variable_group.variables["OVERRIDE_RELEASE_DATE"].value = "2025-10-23" # None
 
-    # if not last_release_run_date_str:
-    #     print("##vso[task.logissue type=error]lastReleaseRunDate is required")
-    #     sys.exit(1)
+        variable_group_parameters = VariableGroupParameters(
+                name=variable_group.name,
+                description=variable_group.description,
+                type=variable_group.type,
+                variables=variable_group.variables,
+                variable_group_project_references=[
+                    VariableGroupProjectReference(
+                         name=variable_group.name,
+                        project_reference=ProjectReference(
+                            id="ede58de8-5d83-4b61-9288-35b324e47e30",
+                            name="athena"
+                        )
+                    )
+                ]
+            )
+        
+        # it removes the description
+        updated_vars = task_agent_client.update_variable_group(variable_group_parameters=variable_group_parameters, group_id=46)
 
-    # last_release_run_date = parse_date(last_release_run_date_str)
-
-    # # Check for override
-    # if override_date_str:
-    #     override_date = parse_date(override_date_str)
-
-    #     if today == override_date:
-    #         print(f"Override date matched today ({today}), pipeline will run ✅")
-    #         sys.exit(0)  # continue pipeline
-    #     elif today == last_release_run_date + timedelta(days=14):
-    #         print(f"Today is the regular 2-week run date ({today}), but override is set -> skipping ❌")
-    #         sys.exit(1)  # skip pipeline
-    #     else:
-    #         print(f"Override date ({override_date}) does not match today ({today}), skipping ❌")
-    #         sys.exit(1)  # skip pipeline
-
-    # # No override -> check bi-weekly Wednesday rule
-    # next_regular_run = last_release_run_date + timedelta(days=14)
-
-    # if today == next_regular_run and today.weekday() == 2:  # Wednesday = 2
-    #     print(f"Today ({today}) is the regular 2nd Wednesday, pipeline will run ✅")
-    #     sys.exit(0)
-    # else:
-    #     print(f"Today ({today}) is not the scheduled run date ({next_regular_run}), skipping ❌")
-    #     sys.exit(1)
+        print(updated_vars)
 
 if __name__ == "__main__":
     main()
